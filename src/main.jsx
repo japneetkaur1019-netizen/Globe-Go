e70bf131a284e85128e9a1fad06a1566b874c6e6
Ridhima <ridhima2912.beai25@chitkara.edu.in>
2026-08-22 16:28:24 +0530

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,56 @@ import { AppProvider } from './context/AppContext.jsx'
 import { WishlistProvider } from './context/WishlistContext.jsx'
 import { FlightBookingProvider } from './context/FlightBookingContext.jsx'
 
-createRoot(document.getElementById('root')).render(
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <AppProvider>
+//         <App />
+//       </AppProvider>
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
+import App from "./App.jsx";
+
+import { AppProvider } from "./context/AppContext.jsx";
+import { WishlistProvider } from "./context/WishlistContext.jsx";
+import { FlightBookingProvider } from "./context/FlightBookingContext.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+
       <AppProvider>
+<<<<<<< HEAD
         <WishlistProvider>
           <FlightBookingProvider>
             <App />
           </FlightBookingProvider>
         </WishlistProvider>
+=======
+
+        <WishlistProvider>
+          <FlightBookingProvider>
+
+            <App />
+
+          </FlightBookingProvider>
+        </WishlistProvider>
+
+>>>>>>> f8ed227 (update main)
       </AppProvider>
+
     </BrowserRouter>
+<<<<<<< HEAD
   </StrictMode>,
 )
 
+=======
+  </StrictMode>
+);
+>>>>>>> f8ed227 (update main)
